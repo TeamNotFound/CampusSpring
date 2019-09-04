f98ec4fe4b233104689754bcb80e731a743655a1
vanny96 <giovannibarbaro7@gmail.com>
2019-09-04 16:33:12 +0200

    First commit

---
+++ src/main/java/teamNotFound/CampusSpringApplication.java
@@ -0,0 +1,13 @@
+package teamNotFound;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampusSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampusSpringApplication.class, args);
+	}
+
+}
