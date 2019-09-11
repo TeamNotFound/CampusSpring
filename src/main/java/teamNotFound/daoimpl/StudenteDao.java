42999cbb8322a926eb91c0d5110a44ee61b6808f
Nicola <Nicola@DESKTOP-E6H5KCP>
2019-09-11 12:51:51 +0200

    controller cattedra e jsp annesse

--- src/main/java/teamNotFound/daoimpl/StudenteDao.java
+++ src/main/java/teamNotFound/daoimpl/StudenteDao.java
@@ -8,14 +8,12 @@ import teamNotFound.dao.CRUDInterface;
 import teamNotFound.dao.CrudGenerico;
 import teamNotFound.model.Studente;
 
-<<<<<<< HEAD
-import teamNotFound.dao.CRUDInterface;
-import teamNotFound.dao.CrudGenerico;
-import teamNotFound.model.Studente;
 
-=======
+
+
+
 @Repository
->>>>>>> branch 'master' of https://github.com/nykomer/CampusSpring
+
 public class StudenteDao extends CrudGenerico<Studente, Integer> implements CRUDInterface<Studente , Integer>{
  
 
@@ -25,11 +23,9 @@ this.classeT=Studente.class;	}
 	
 	
 	public Studente getByIdWithPrenotazioni(int id) {
-<<<<<<< HEAD
+
 		Studente studente =entity.find(Studente.class, id);
-=======
-		Studente studente = entity.find(Studente.class, id);
->>>>>>> branch 'master' of https://github.com/nykomer/CampusSpring
+
 		studente.getPrenotazioni().size();
 		return studente;
 	}
@@ -45,10 +41,9 @@ this.classeT=Studente.class;	}
 		studente.getPrenotazioni().size();
 		studente.getEsami().size();
 		return studente;
-<<<<<<< HEAD
 
-=======
->>>>>>> branch 'master' of https://github.com/nykomer/CampusSpring
+
+
 	}
 
 	
