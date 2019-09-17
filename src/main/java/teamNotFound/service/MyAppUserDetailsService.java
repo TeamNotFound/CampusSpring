e6db8af13f383c766fef7606d5cbd45f28a9e346
vanny96 <giovannibarbaro7@gmail.com>
2019-09-17 12:15:50 +0200

    Solve merge conflicts

--- src/main/java/teamNotFound/service/MyAppUserDetailsService.java
+++ src/main/java/teamNotFound/service/MyAppUserDetailsService.java
@@ -1,48 +1,35 @@
 package teamNotFound.service;
 
-import java.util.Arrays;
-
-import org.slf4j.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import teamNotFound.daoimpl.AccountDao;
 import teamNotFound.model.Account;
+@Service
+public class MyAppUserDetailsService implements UserDetailsService{
+	@Autowired
+	private AccountDao accountDao;
 
-//Service che recupera le informazione dell utente dal database
-	@Service
-	//Poich  la classe   annotata con @Service, Spring provveder  a crearne un istanza nell application context. 
-	//Spring Security rilever  la presenza di un istanza  concreta  di UserDetailsService nel contesto dell applicazione e
-	//utilizzer  il nuovo servizio per recuperare i dati necessari all autenticazione.
-	public class MyAppUserDetailsService implements UserDetailsService {
-		@Autowired
-		private AccountDao dao;
-		//Assegnamo la classe al nostro file di log
-		private static final Logger logger = LoggerFactory.getLogger(MyAppUserDetailsService.class);	
-		//PRENDE LE INFORMAZIONI DALLA FORM SE NON  SPECIFICATA DA FORM LOGIN PRENDE LE INFO DALLA FORM DI DEFAULT
-		//SE SPECIFICATA LA FORM TRAMITE FORMLOGIN PRENDE LE INFORMAZIONI DA FORMLOGIN
-		@Override
-		public UserDetails loadUserByUsername(String username) {
-			UserDetails userDetails = null;
-			try {
-				//Prende un account tramite username dal database
-				Account acc = dao.getByUsername(username);
-				//Assegna attraverso un oggetto GrantedAuthority l'autorit  dell'utente trovato, nel mio caso
-				//viene assegnato un ruolo che pu  essere una stringa fra Amministratore,God,Utente.
-				GrantedAuthority authority = new SimpleGrantedAuthority(acc.getRuolo().getRuolo());
-				//Assegnamo a userDetails i dettagli dell'utente trovato ( Es. Admin admin) e trattiamo l'autorit  come una lista
-				userDetails = (UserDetails) new User(acc.getUsername(), acc.getPassword(), Arrays.asList(authority));
-				//Se l'utente non viene trovato, o una di queste operazioni non dovrebbe andare a buon fine andremo nel catch
-			} catch (Exception e) {
-				logger.warn("Utente non trovato");
-			}
-			//ritorniamo i dettagli dell'utente oramai riempito con username, password e una lista di autorit 
-			return userDetails;
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException{
+		System.out.println("\n\nInside userdetails");
+		Account account = accountDao.getByUsername(username);
+		if(account != null) {
+			System.out.println("Found account\n\n");
+			System.out.println(account.getRuolo());
+			UserDetails user = User.withUsername(account.getUsername()).password(account.getPassword())
+												.roles(account.getRuolo().getRuolo()).build();
+			System.out.println("builded");
+			return user;
+		}else {
+			throw new UsernameNotFoundException("USER NOT FOUND!");
 		}
-	} 
+	}
+} 
 
