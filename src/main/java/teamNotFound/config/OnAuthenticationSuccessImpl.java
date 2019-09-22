bec2d2483ec804dc2ca3d608057f2ab38f4b7fe8
Giovanni Barbaro <giovannibarbaro7@gmail.com>
2019-09-22 23:50:27 +0200

    Add redirect to authentication success impl

--- src/main/java/teamNotFound/config/OnAuthenticationSuccessImpl.java
+++ src/main/java/teamNotFound/config/OnAuthenticationSuccessImpl.java
@@ -8,30 +8,26 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 
 import teamNotFound.daoimpl.AccountDao;
 
 @Component
-public class OnAuthenticationSuccessImpl implements AuthenticationSuccessHandler {
+public class OnAuthenticationSuccessImpl extends SavedRequestAwareAuthenticationSuccessHandler {
 	@Autowired
 	private AccountDao accountDao;
 	@Autowired
 	private AmazonUploadUtil amazonUtil;
 	
-	SavedRequestAwareAuthenticationSuccessHandler auth;
-
 	@Override
 	public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
 			Authentication authentication) throws IOException, ServletException {
 		
 		String generatedKey = accountDao.getByUsername(authentication.getName()).getUtente().getImageGeneratedName();
-		
 		request.getSession().setAttribute("profilePic", amazonUtil.generateUrl(generatedKey));
 		
-		response.sendRedirect(request.getContextPath());
+		super.onAuthenticationSuccess(request, response, authentication);
 	}
 
 }
